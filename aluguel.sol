94bcc8b7849c6e47b8215cfb95332db5141755a0
gabrielguarento <158359650+gabrielguarento@users.noreply.github.com>
2024-01-31 19:25:12 -0300

    .

--- aluguel.sol
+++ aluguel.sol
@@ -1,6 +1,6 @@
 /*
 SPDX-License-Identifier: CC-BY-4.0
-(c) Desenvolvido por Jeff Prestes
+(c) Desenvolvido por Jeff Prestes -
 This work is licensed under a Creative Commons Attribution 4.0 International License.
 */
 pragma solidity 0.8.19;
